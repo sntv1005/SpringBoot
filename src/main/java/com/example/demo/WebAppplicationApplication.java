3bb3c11b49090797555b7c00e12445fe03d00cb7
Srinath V <srinathv35@gmail.com>
2023-07-06 15:24:04 +0530

    committed

---
+++ src/main/java/com/example/demo/WebAppplicationApplication.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppplicationApplication.class, args);
+
+		//	System.out.println("Srinathv");
+		
+		
+	}
+
+}
---
+++ src/test/java/com/example/demo/WebAppplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
